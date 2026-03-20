05ccf01727c454357fa912930611698603cc1bf7
Saransh8520 <saranshpillai16@gmail.com>
2026-03-20 16:11:55 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, jsonify, render_template, redirect
+from db import get_connection
+
+app = Flask(__name__)
+
+# HOME PAGE
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# GET TASKS
+@app.route('/tasks')
+def get_tasks():
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM tasks")
+    tasks = cursor.fetchall()
+
+    return jsonify(tasks)
+
+# ADD TASK
+@app.route('/add', methods=['POST'])
+def add_task():
+    title = request.form['title']
+    description = request.form['description']
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO tasks(title, description) VALUES (%s, %s)", (title, description))
+    conn.commit()
+
+    return redirect('/')
+
+# DELETE TASK
+@app.route('/delete')
+def delete():
+    id = request.args.get('id')
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM tasks WHERE id=%s", (id,))
+    conn.commit()
+
+    return "Deleted"
+
+# COMPLETE TASK
+@app.route('/complete')
+def complete():
+    id = request.args.get('id')
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("UPDATE tasks SET status='COMPLETED' WHERE id=%s", (id,))
+    conn.commit()
+
+    return "Updated"
+
+if __name__ == '__main__':
+    app.run(debug=True)
